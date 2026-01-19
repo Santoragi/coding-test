305f95a89ec989de7e43855f817a342f934d752c
장우진 <woojj1254577@naver.com>
2026-01-19 16:19:28 +0900

    [Gold V] Title: 회의실 배정, Time: 464 ms, Memory: 41788 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
 
-        int[][] time = new int[n][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+             @Override
+             public int compare(int[] o1, int[] o2) {
+                 if(o1[1] == o2[1]) {
+                     return o1[0] - o2[0];
+                 }
 
+                 return o1[1] - o2[1];
+             }
+        });
+
+        StringTokenizer st;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            time[i][0] = Integer.parseInt(st.nextToken());
-            time[i][1] = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{start, end});
         }
-        
-        Comparator<int[]> comp = new Comparator<>() {
-            
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                
-                if(o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
-                }
-                
-                return o1[1] - o2[1];
-            }
-        };
-
-        Arrays.sort(time, comp);
 
         int cnt = 0;
-        int prev_end_time = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(prev_end_time <= time[i][0]) {
-                prev_end_time = time[i][1];
+        int prev_end = 0;
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if(cur[0] >= prev_end) {
                 cnt++;
+                prev_end = cur[1];
             }
         }
 
