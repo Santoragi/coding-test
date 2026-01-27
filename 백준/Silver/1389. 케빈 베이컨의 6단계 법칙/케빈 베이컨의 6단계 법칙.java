f7bcd9837c03f47b4a3a900314b216a29eac1fef
장우진 <woojj1254577@naver.com>
2026-01-27 13:45:31 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 272 ms, Memory: 63116 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] relation;
+    static boolean[] visited;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        relation = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            relation[A][B] = 1;
+            relation[B][A] = 1;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int idx = 0;
+        for(int i = 1; i <= N; i++) {
+            int sum = 0;
+            for(int j = 1; j <= N; j++) {
+                if(i == j) continue;
+                int result = bfs(i, j);
+                sum += result;
+            }
+
+            if(sum < min) {
+                min = sum;
+                idx = i;
+            }
+        }
+
+        System.out.println(idx);
+    }
+
+    static int bfs(int start, int target) {
+        Queue<int[]> q = new LinkedList<>();
+        visited = new boolean[N + 1];
+
+        q.add(new int[]{start, 0});
+        visited[start] = true;
+
+        int result = 0;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int idx = cur[0];
+            int cnt = cur[1];
+
+            if(idx == target) {
+                result = cnt;
+            }
+
+            for(int i = 1; i <= N; i++) {
+                if(i == idx) continue;
+                if(relation[idx][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    q.add(new int[]{i, cnt + 1});
+                }
+            }
+        }
+
+        return result;
+    }
+}
