09ada465abe5810d21a0f3537b57c0266d964a42
장우진 <woojj1254577@naver.com>
2026-02-19 17:16:24 +0900

    [level 2] Title: 가장 큰 수, Time: 186.69 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        
+        String[] arr = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            String s = String.valueOf(numbers[i]);
+            arr[i] = s;
+        }
+        
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if(arr[0].equals("0")) {
+            return "0";
+        }
+        
+        for(String s : arr) {
+            sb.append(s);
+        }
+        
+        return sb.toString();
+    }
+}
