0ffb2f70ede07f2ae1b8ff31ae413b449c2added
장우진 <woojj1254577@naver.com>
2026-02-24 20:27:58 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 41988 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,44 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static int N;
+    static PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+        public int compare(int[] o1, int[] o2) {
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        }
+    });
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-             @Override
-             public int compare(int[] o1, int[] o2) {
-                 if(o1[1] == o2[1]) {
-                     return o1[0] - o2[0];
-                 }
-
-                 return o1[1] - o2[1];
-             }
-        });
-
         StringTokenizer st;
-        for(int i = 0; i < n; i++) {
+
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            pq.add(new int[]{start, end});
+
+            pq.offer(new int[]{start, end});
         }
 
         int cnt = 0;
-        int prev_end = 0;
+        int last = 0;
         while(!pq.isEmpty()) {
             int[] cur = pq.poll();
-            if(cur[0] >= prev_end) {
+
+            if(cur[0] >= last) {
                 cnt++;
-                prev_end = cur[1];
+                last = cur[1];
             }
         }
 
