aa9ad1df37f434c07abc432600b5213e3fafa96b
장우진 <woojj1254577@naver.com>
2026-02-24 17:45:19 +0900

    [level 2] Title: 짝지어 제거하기, Time: 69.86 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        char[] arr = s.toCharArray();
+        
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> temp = new Stack<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(stack.isEmpty()) {
+                stack.push(arr[i]);
+                continue;
+            }
+
+            if(arr[i] == stack.peek()) {
+                stack.pop();
+            }
+            else {
+                stack.push(arr[i]);
+            }
+        }
+        
+        int result = 0;
+        if(stack.isEmpty()) result = 1;
+        
+        return result;
+    }
+}
