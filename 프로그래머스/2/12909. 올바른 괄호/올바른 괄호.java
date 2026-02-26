27749c267f0bde55845fb6d70cbcd2b9702d3516
장우진 <woojj1254577@naver.com>
2026-02-26 17:01:29 +0900

    [level 2] Title: 올바른 괄호, Time: 18.80 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
-
+        boolean answer = true;
+        
         Stack<Character> stack = new Stack<>();
+        
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if(c == '(') {
                 stack.push(c);
             }
             else {
-                if(stack.isEmpty()) return false;
+                if(stack.isEmpty()) {
+                    answer = false;
+                    break;
+                }
                 stack.pop();
             }
         }
         
-        if(stack.isEmpty()) {
-            answer = true;
-        }
-        
+        if(!stack.isEmpty()) answer = false;
+
         return answer;
     }
 }
